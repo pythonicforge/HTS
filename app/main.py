bcc59eca5f47de53cc3fb80963db92e4768a9500
pythonicforge <pythonicforge@gmail.com>
2025-11-20 14:26:40 +0000

    added: cd, type and exec commands

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 import sys
+import os
 import shlex
 import subprocess
 
+def find_path(cmd):
+    paths = os.environ.get("PATH", "").split(os.pathsep)
+    for p in paths:
+        full = os.path.join(p, cmd)
+        if os.path.isfile(full) and os.access(full, os.X_OK):
+            return full
+    return None
 
 def App():
+
+    BUILTINS = ["exit", "echo", "cd", "type"]
 
     while True:
         try:
@@ -39,14 +49,43 @@ def App():
         elif cmd == "echo":
             print(" ".join(tokens[1:]))
 
+        elif cmd == "cd":
+            target = ""
+            if len(tokens) < 2:
+                target = os.path.expanduser("~")
+            else:
+                target = tokens[1]
+
+            try:
+                os.chdir(target)
+            except Exception as e:
+                print(e)
+
+        elif cmd == "type":
+            if len(tokens) < 2:
+                print("type: missing argument")
+                continue
+
+            target = tokens[1]
+
+            if target in BUILTINS:
+                print(f"{target} is a shell builtin")
+                continue
+
+            path = find_path(target)
+            if path:
+                print(f"{target} is {path}")
+            else:
+                print(f"{target}: not found")
+
+
         else:
-            # If command is not found treat it as an external command
             try:
                 subprocess.run(tokens)
             except FileNotFoundError:
                 print(f"{tokens[0]}: command not found")
             except Exception as e:
-                print(f"error running command: {e}")
+                print(f"{e}: ran into an error. Please try again later.")
 
 
 if __name__ == "__main__":
